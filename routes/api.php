628d914797e1098567d2ee522473a1ba7e81124e
Swayam <subinghi22@gmail.com>
2025-08-31 19:43:32 +0545

    Created own middleware

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\V1\TourController;
 use App\Http\Controllers\Api\V1\TravelController;
 use App\Http\Controllers\LoginController;
+use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,7 +15,7 @@ Route::prefix('v1')->group(function () {
     Route::get('/travels', [TravelController::class, 'index']);
     Route::get('/travels/{travel:slug}/tours', [TourController::class, 'index']);
     Route::post('/login', LoginController::class);
-    Route::prefix('admin')->middleware(['auth:sanctum', 'role:admin'])->group(function () {
+    Route::prefix('admin')->middleware(['auth:sanctum', RoleMiddleware::class.':admin'])->group(function () {
         Route::post('travels', [TravelController::class, 'store']);
     });
-});
+});// :admin is the parameter to that alias
